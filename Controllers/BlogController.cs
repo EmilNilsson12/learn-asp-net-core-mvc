d6e22ac882ab33e24c777e57b821cfc517bece71
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-09-22 10:51:54 +0200

    Add new class BlogController

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCaliforniaApp.Controllers
+{
+    public class BlogController
+    {
+        // GET: /<controller>/
+        public string Index()
+        {
+            return "BlogController!";
+        }
+    }
+}
