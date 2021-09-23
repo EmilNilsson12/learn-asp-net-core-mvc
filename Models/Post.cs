2065e352254a83c2f14336f0764376f56bbe54ea
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-09-23 11:40:07 +0200

    Add validation rules to Post model

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ExploreCaliforniaApp.Models
 {
     public class Post
     {
+        [Required]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Title must be between 5 and 100 characters long")]
         public string Title { get; set; }
+
         public string Author { get; set; }
+        [Required]
+        [MinLength(100, ErrorMessage = "Blog posts must be at least 100 characters long")]
         public string Body { get; set; }
         public DateTime Posted { get; set; }
     }
