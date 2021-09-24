f1434bbcb70d874a4dd4ec4d205446662ff06657
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-09-24 14:42:36 +0200

    Add DataType attribute to form input elements

--- ExploreCaliforniaApp/Models/Post.cs
+++ ExploreCaliforniaApp/Models/Post.cs
@@ -7,11 +7,14 @@ namespace ExploreCaliforniaApp.Models
 {
     public class Post
     {
+        [Display(Name = "Post Tilte")]
         [Required]
+        [DataType(DataType.Text)]
         [StringLength(100, MinimumLength = 5, ErrorMessage = "Title must be between 5 and 100 characters long")]
         public string Title { get; set; }
         public string Author { get; set; }
         [Required]
+        [DataType(DataType.MultilineText)]
         [MinLength(100, ErrorMessage = "Blog posts must be at least 100 characters long")]
         public string Body { get; set; }
         public DateTime Posted { get; set; }
